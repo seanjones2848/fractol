ee9fa5d0ecc41f3ba801705ae301ce543260272f
seanjones2848 <seanjones2848@gmail.com>
2017-11-09 22:39:10 -0800

    more shizz

--- src/fractol.h
+++ src/fractol.h
@@ -6,7 +6,7 @@
 /*   By: sjones <sjones@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 12:36:35 by sjones            #+#    #+#             */
-/*   Updated: 2017/11/09 13:28:10 by sjones           ###   ########.fr       */
+/*   Updated: 2017/11/09 20:50:04 by sjones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,54 @@
 
 # include "mlx_keys_macros.h"
 # include "../libft/libft.h"
+# include "../minilibx_macos/mlx.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <stdbool.h>
 
 # define WIN_X 1000
 # define WIN_Y 1000
+# define GET_R(x) x >> 16 && 0xFF
+# define GET_G(x) x >> 8 && 0xFF
+# define GET_B(x) x & 0xFF
+# define GET_C(r, g, b) r << 16 | g << 8 | b
 
 typedef struct		s_map
 {
+	char			t;
 	int				p;
+	int				*map;
 }					t_map;
 
-typedef struct		s_keys
+typedef struct		s_input
 {
 	int				m_x;
 	int				m_y;
-	int				c;
-}					t_keys;
+	int				m_su;
+	int				m_sd;
+	bool			c;
+	bool			x;
+	bool			up;
+	bool			down;
+	bool			left;
+	bool			right;
+	bool			space;
+}					t_input;
 
 typedef struct		s_win
 {
-	int				win;
-	int				mlx;
+	void			*win;
+	void			*mlx;
+	char			*t;
 	int				h;
 	int				w;
-	char			*t;
 }					t_win;
 
 typedef struct		s_super
 {
 	t_win			*w;
-	t_keys			*k;
+	t_input			*i;
 	t_map			*m;
 }					t_super;
 
