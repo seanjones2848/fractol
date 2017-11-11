30c415bf6f181d152a08c78b3fb0470577b6af1d
Ross Levine <magicbyross@gmail.com>
2017-11-10 19:40:34 -0800

    wooo

--- src/init.c
+++ src/init.c
@@ -6,29 +6,15 @@
 /*   By: rlevine <rlevine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 13:06:36 by sjones            #+#    #+#             */
-/*   Updated: 2017/11/10 19:20:02 by rlevine          ###   ########.fr       */
+/*   Updated: 2017/11/10 19:40:23 by rlevine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-t_super			*init_super(int p)
-{
-	t_super	*s;
-
-	if (!(s = (t_super*)malloc(sizeof(t_super))))
-	{
-		write(1, "malloc failed on t_super\n", 25);
-		return NULL;
-	}
-	s->w = init_win(void);
-	s->k = init_keys(void);
-	s->m = init_map(p);
-}
-
 static t_win	*init_win(void)
 {
-	t_win *w;
+	t_win	*w;
 
 	if(!(w = (t_win*)malloc(sizeof(t_win))))
 	{
@@ -39,5 +25,49 @@ static t_win	*init_win(void)
 	w->w = WIN_X;
 	w->h = WIN_Y;
 	w->mlx = mlx_init();
-	w->win = mlx_new_window(w->mlx, w->w, w->h, win->t);
+	w->win = mlx_new_window(w->mlx, w->w, w->h, w->t);
+	return (w);
+}
+
+static t_input	*init_input(void)
+{
+	t_input	*i;
+
+	if (!(i = (t_input*)malloc(sizeof(t_input))))
+	{
+		write(1, "malloc failed on t_input\n", 25);
+		return NULL;
+	}
+	ft_bzero(i, sizeof(t_input));
+	return (i);
+}
+
+static t_map	*init_map(char t, int p)
+{
+	t_map	*m;
+
+	if (!(m = (t_map*)malloc(sizeof(t_map))))
+	{
+		write(1, "malloc failed on t_map\n", 23);
+		return NULL;
+	}
+	ft_bzero(m, sizeof(t_map));
+	m->t = t;
+	m->p = p;
+	return (m);
+}
+
+t_super			*init_super(char t, int p)
+{
+	t_super	*s;
+
+	if (!(s = (t_super*)malloc(sizeof(t_super))))
+	{
+		write(1, "malloc failed on t_super\n", 25);
+		return NULL;
+	}
+	s->w = init_win();
+	s->i = init_input();
+	s->m = init_map(t, p);
+	return (s);
 }
